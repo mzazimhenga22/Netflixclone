3637144fd9231212d2a045af61d934d515cb690d
Vives CHarris <vivescharris8@gmail.com>
2025-10-06 22:38:22 +0000

    the card starts when the number is ending and make sure only one card ho

--- src/components/browse/Top10Card.tsx
+++ src/components/browse/Top10Card.tsx
@@ -16,7 +16,7 @@ const Top10Card = ({ movie, rank }: Top10CardProps) => {
     : `https://picsum.photos/seed/${movie.id}/400/600`;
 
   return (
-    <div className="group relative flex items-center h-full cursor-pointer">
+    <div className="group flex items-center h-full cursor-pointer">
       <div 
         className="text-[200px] font-black text-transparent transition-transform duration-300 ease-in-out group-hover:scale-105" 
         style={{
@@ -26,7 +26,7 @@ const Top10Card = ({ movie, rank }: Top10CardProps) => {
       >
         {rank}
       </div>
-      <div className="absolute left-8 w-[140px] h-[210px] rounded-md overflow-hidden transition-transform duration-300 ease-in-out group-hover:scale-110 shadow-lg">
+      <div className="relative -ml-8 w-[140px] h-[210px] rounded-md overflow-hidden transition-transform duration-300 ease-in-out group-hover:scale-110 shadow-lg">
         <Image
           src={posterUrl}
           alt={movie.title || movie.name || 'Movie poster'}
