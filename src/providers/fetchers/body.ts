1aa2914d0946e78cd7baadc16042a323d6fd0da6
Vives CHarris <vivescharris8@gmail.com>
2025-10-07 15:04:31 +0000

    Try fixing this error: `Build Error: Module not found: Can't resolve 'pr

---
+++ src/providers/fetchers/body.ts
@@ -0,0 +1,25 @@
+import FormData from 'form-data';
+
+import { FetcherOptions } from './types';
+
+export interface SeralizedBody {
+  headers: Record<string, string>;
+  body: FormData | URLSearchParams | string | undefined;
+}
+
+export function serializeBody(body: FetcherOptions['body']): SeralizedBody {
+  if (body === undefined || typeof body === 'string' || body instanceof URLSearchParams || body instanceof FormData) {
+    return {
+      headers: {},
+      body,
+    };
+  }
+
+  // serialize as JSON
+  return {
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(body),
+  };
+}
