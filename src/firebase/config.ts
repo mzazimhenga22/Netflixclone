5a8e95f2ddaf3dfd367c81594a8247ec08dd6589
Vives CHarris <vivescharris8@gmail.com>
2025-10-07 00:55:05 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-9973247152-e6670",
+  "appId": "1:83259042827:web:cf1c529bd6dea207b72b0e",
+  "apiKey": "AIzaSyB46DPw-Qb2trz3YrjXwNU3t96OVL59R2c",
+  "authDomain": "studio-9973247152-e6670.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "83259042827"
+};
